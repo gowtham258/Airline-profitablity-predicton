0336d45e5a18f3e1eb198c75ced8ee37615de725
gowtham258 <gowthamaryan405@gmail.com>
2025-04-17 22:57:34 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -35,7 +35,7 @@ def load_data():
 df = load_data()
 
 # Dashboard content continues...
-pip install matplotlib
+# pip install matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
